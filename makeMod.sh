e8f33a2ef90a9daefb6ca18c2bfc4c445a657055
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2013-06-05 16:40:05 +0300

    ff build script added

---
+++ makeMod.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SDK_DIR=$1
+SDK_DIR=/Users/sg/Documents/cod4mw_modtools_v1
+SRC_DIR=`pwd`
+OUT_DIR=build/tactical
+[ -e ${OUT_DIR}/mod.ff ] && rm -f ${OUT_DIR}/mod.ff
+#Any additional resources should be moved in to appropriate folder inside $SDK_DIR/raw 
+cp -rf ./ui_mp/* "${SDK_DIR}/raw/ui_mp/"
+
+cp -f mod.csv "${SDK_DIR}/zone_source/"
+cd "${SDK_DIR}/bin/"
+wine linker_pc.exe -language english -compress -cleanup mod
+
+cp -f "${SDK_DIR}/zone/english/mod.ff" "${SRC_DIR}/${OUT_DIR}/mod.ff"
+
+
